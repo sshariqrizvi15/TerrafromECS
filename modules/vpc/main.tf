80a1dc3b3b9a9768f1fe5af2a6751e52bebd3cd9
Shariq <shariq@Shariqs-MacBook-Pro.local>
2019-07-15 22:41:57 +0500

    Updated with dynamic port mapping

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -120,6 +120,14 @@ resource "aws_security_group" "test_public_sg" {
       "10.0.1.0/24"]
   }
 
+  ingress {
+    from_port = 0
+    to_port = 65535
+    protocol = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"]
+  }
+
   egress {
     # allow all traffic to private SN
     from_port = "0"
